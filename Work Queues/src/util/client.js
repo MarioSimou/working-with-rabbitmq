0a8b4d944b0ae1ce784411eb88a316777cc74df5
MarioSimou <marios.simouu@gmail.com>
2019-07-28 17:21:48 +0100

    basic tempalte

---
+++ Work Queues/src/util/client.js
@@ -0,0 +1,15 @@
+import amqp from 'amqplib/callback_api'
+let client
+
+export default {
+    connect: ({ uri }) => new Promise((resolve,reject)=> {
+        amqp.connect( uri , ( e , conn ) => {
+            console.log(conn)
+            if( e ) reject ( e ) 
+
+            client = conn
+            resolve( conn )
+        })
+    }),
+    client : client
+}
